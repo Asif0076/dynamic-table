decbe87f3d9da55766e825d0b8af78db09ed4e5a
Asif Nawaz <assu.nawaz007@gmail.com>
2023-02-15 12:05:04 +0500

    set routes to table components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import TableForm from './component/TableForm';
+import TableList from './component/TableList';
+import SingleTable from './component/SingleTable';
 
 function App() {
   return (
-    <div className="App">
-  <h1>Hello World!!</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/create-table">Create Table</Link>
+              </li>
+              <li>
+                <Link to="/tables">All Tables</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/create-table">
+              <TableForm/>
+            </Route>
+            <Route path="/tables">
+              <TableList/>
+            </Route>
+            <Route path="/table/:id">
+              <SingleTable />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
